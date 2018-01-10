bf52d37ad0e33526a92365c79ff2470f28ab1a28
Mateusz <gorzanski.mateusz@gmail.com>
2018-01-10 16:00:01 +0100

    update header

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import { Button, DropdownToggle, DropdownMenu, DropdownItem, UncontrolledDropdown } from 'reactstrap';
+import fontawesome from '@fortawesome/fontawesome';
+import faLock from '@fortawesome/fontawesome-free-solid/faLock';
+fontawesome.library.add(faLock);
 
 class Header extends React.Component {
     render() {
@@ -12,13 +15,15 @@ class Header extends React.Component {
                     </section>
 
                     <nav className="page-nav">
+                        <Button color="primary" className="page-nav-unlock-btn"><i className="fas fa-lock"></i> Unlock</Button>
                         <Route render={({ history }) => (
-                        <Button color="primary" className="page-nav-new-btn" onClick={() => { history.push('/new') }}>Add new entry</Button>
+                            <Button color="primary" className="page-nav-new-btn" onClick={() => { history.push('/new') }}>Add new entry</Button>
                         )} />
                         <UncontrolledDropdown className="page-nav-dropdown-menu">
                             <DropdownToggle nav caret>
                             </DropdownToggle>
                             <DropdownMenu>
+                                <DropdownItem><Link to="/settings">Settings</Link></DropdownItem>
                                 <DropdownItem><Link to="/logout">Logout</Link></DropdownItem>
                             </DropdownMenu>
                         </UncontrolledDropdown>
