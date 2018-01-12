3e196a637950c8c9b2d2eea689049f0712ebb98d
Mateusz <gorzanski.mateusz@gmail.com>
2018-01-12 10:15:59 +0100

    fix console errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 //import PropTypes from 'prop-types';
 import Header from './components/header';
 import Home from './pages/home';
-import Error404 from './pages/errors/404';
+//import Error404 from './pages/errors/404';
 import NewEntry from './pages/new-entry';
 
 class App extends Component {
