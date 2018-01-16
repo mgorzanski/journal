b9f299df3d701bcda30f4dd1b39353a822e0bc00
Mateusz <gorzanski.mateusz@gmail.com>
2018-01-16 20:08:01 +0100

    fix useless Callback constructor bug

--- client/src/components/Callback.js
+++ client/src/components/Callback.js
@@ -2,10 +2,6 @@ import { Component } from 'react';
 import { setIdToken, setAccessToken } from './../utils/AuthService';
 
 class Callback extends Component {
-    constructor() {
-        super();
-    }
-
     componentDidMount() {
         setAccessToken();
         setIdToken();
